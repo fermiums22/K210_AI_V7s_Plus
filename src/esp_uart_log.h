e222608e423b5dcb9e79e8a9389d08ebaa635516
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-03 19:37:51 +0300

    Expose ESP UART bridge state for quiet SD reads

--- src/esp_uart_log.h
+++ src/esp_uart_log.h
@@ -2,6 +2,7 @@
 
 void esp_uart_log_start(void);
 void esp_uart_log_stop(void);
+int esp_uart_log_is_started(void);
 int esp_uart_log_spi_ready(void);
 int esp_uart_log_put_active(void);
 void esp_uart_log_clear_put_active(void);
