94f0c524633dedb1751de1bdfa775ed8880265b0
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-03 19:24:32 +0300

    Expose ESP UART log bridge control

--- src/esp_uart_log.h
+++ src/esp_uart_log.h
@@ -1,6 +1,7 @@
 #pragma once
 
 void esp_uart_log_start(void);
+void esp_uart_log_stop(void);
 int esp_uart_log_spi_ready(void);
 int esp_uart_log_put_active(void);
 void esp_uart_log_clear_put_active(void);
