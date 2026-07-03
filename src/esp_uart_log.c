c0c45dc283c78cd8cd164a0eb69086caf211d270
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-03 13:02:35 +0300

    Use ESP boot and app UART baud rates

--- src/esp_uart_log.c
+++ src/esp_uart_log.c
@@ -9,6 +9,9 @@
 #include <gpio.h>
 #include <sysctl.h>
 
+#define ESP_BOOT_BAUD 74880
+#define ESP_APP_BAUD  115200
+
 static volatile gpio_t *const REG_GPIO = (volatile gpio_t *)GPIO_BASE_ADDR;
 
 static void gpio_set(int n, int v)
@@ -27,7 +30,6 @@ static void esp_reset_normal(void)
     gpio_set(GPIO_ESP_EN, 0);
     vTaskDelay(pdMS_TO_TICKS(120));
     gpio_set(GPIO_ESP_EN, 1);
-    vTaskDelay(pdMS_TO_TICKS(300));
 }
 
 static void esp_uart_log_task(void *arg)
@@ -54,8 +56,17 @@ void esp_uart_log_start(void)
         LOG("[esp-uart] open failed");
         return;
     }
-    uart_config(u, 115200, 8, UART_STOP_1, UART_PARITY_NONE);
+
+    /* ESP8266/ESP8285 ROM boot log is 74880 baud.  Our Arduino firmware uses
+     * Serial.begin(115200).  Capture the ROM at boot baud, then switch to the
+     * application baud before normal kesp logs start. */
+    uart_config(u, ESP_BOOT_BAUD, 8, UART_STOP_1, UART_PARITY_NONE);
     xTaskCreate(esp_uart_log_task, "esp_uart_log", 512, (void *)u, 1, NULL);
-    LOG("[esp-uart] bridge on 115200");
+    LOG("[esp-uart] bridge boot baud 74880");
+
     esp_reset_normal();
+    vTaskDelay(pdMS_TO_TICKS(350));
+
+    uart_config(u, ESP_APP_BAUD, 8, UART_STOP_1, UART_PARITY_NONE);
+    LOG("[esp-uart] bridge app baud 115200");
 }
