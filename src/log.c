ff33786fc1612fc4b61d00ab28c296e879a620b3
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-03 15:06:15 +0300

    Run debug UART at 921600 baud

--- src/log.c
+++ src/log.c
@@ -4,13 +4,28 @@
 #include <platform.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <sysctl.h>
 #include <uarths.h>
+
+static volatile uarths_t *const REG_UARTHS = (volatile uarths_t *)UARTHS_BASE_ADDR;
+
+static void uarths_set_baud(uint32_t baud)
+{
+    uint32_t freq = sysctl_clock_get_freq(SYSCTL_CLOCK_CPU);
+    uint32_t div = freq / baud;
+    if (div > 0)
+        div--;
+    if (div > 0xffffu)
+        div = 0xffffu;
+    REG_UARTHS->div.div = (uint16_t)div;
+}
 
 void log_init(void)
 {
     fpioa_set_function(PIN_DBG_RX, FUNC_UARTHS_RX);
     fpioa_set_function(PIN_DBG_TX, FUNC_UARTHS_TX);
     uarths_init();
+    uarths_set_baud(APP_LOG_BAUD);
 }
 
 void log_puts(const char *s)
