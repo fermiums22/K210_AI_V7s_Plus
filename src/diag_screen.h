8db2bad205c3c04c1286c3cb80058436aef69a23
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-05 18:09:21 +0300

    Guard diagnostic LCD writes

--- src/diag_screen.h
+++ src/diag_screen.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 
+void diag_screen_set_enabled(int enabled);
 void diag_clear(const char *title);
 void diag_line(int line, const char *text);
 void diag_printf(int line, const char *fmt, ...);
