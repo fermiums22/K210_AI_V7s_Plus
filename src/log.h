69be2707b1a7f975916e48856193b43d6bc70f1d
fermiums22 <118602434+fermiums22@users.noreply.github.com>
2026-07-06 15:00:58 +0300

    Expose UART clock diagnostic log

--- src/log.h
+++ src/log.h
@@ -6,6 +6,7 @@
 void log_init(void);
 void log_puts(const char *s);
 void log_printf(const char *fmt, ...);
+void log_dump_uart_clock(void);
 
 #define LOG(s) log_puts(s)
 #define LOGF(...) log_printf(__VA_ARGS__)
